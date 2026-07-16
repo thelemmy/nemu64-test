lemmy-64/n64-systemtest

Add support for debug prints via SC64 USB interface

src/isviewer.rs
@@ -1,48 +1,50 @@
 use crate::pi::Pi;
 
-/// Write the length of the text here
-const ISVIEWER_WRITE_LEN: *mut u32 = 0xB3FF0014 as *mut u32;
+const WRITE_LEN: *mut u32 = 0xB3FF0014 as *mut u32;
+const BUF: *mut u32 = 0xB3FF0020 as *mut u32;
+const CHUNK: usize = 0x200;
 
-// Write text data into this buffer
-const ISVIEWER_BUFFER_START: *mut u32 = 0xB3FF0020 as *mut u32;
-const ISVIEWER_BUFFER_LENGTH: usize = 0x200;
-
+#[inline(always)]
 fn pi_wait() {
     while Pi::status().io_busy() {}
 }
 
-
-// This method simply prints text without synchronization. This should only be used from within
-// the exception handler which can't wait for a lock
-pub fn text_out(s: &str) {
-    if crate::emux::text_out(s) {
-        return;
-    }
-
-    for chunk in s.as_bytes().chunks(ISVIEWER_BUFFER_LENGTH) {
-        // Write
-        let mut value = 0u32;
-        let mut shift = 24u32;
-        let mut i = 0;
-        for byte in chunk {
-            value |= (*byte as u32) << shift;
-            if shift == 0 {
-                pi_wait();
-                unsafe { ISVIEWER_BUFFER_START.add(i).write_volatile(value) };
-                i += 1;
-                shift = 24;
-                value = 0;
-            } else {
-                shift -= 8;
-            }
-        }
-        if shift < 24 {
+fn pack(dst: *mut u32, chunk: &[u8]) {
+    let mut v = 0u32;
+    let mut sh = 24;
+    let mut i = 0usize;
+    for &b in chunk {
+        v |= (b as u32) << sh;
+        if sh == 0 {
             pi_wait();
-            unsafe { ISVIEWER_BUFFER_START.add(i).write_volatile(value) };
+            unsafe { dst.add(i).write_volatile(v) };
+            i += 1;
+            sh = 24;
+            v = 0;
+        } else {
+            sh -= 8;
         }
+    }
+    if sh < 24 {
         pi_wait();
-        unsafe {
-            ISVIEWER_WRITE_LEN.write_volatile(chunk.len() as u32);
-        }
+        unsafe { dst.add(i).write_volatile(v) };
+    }
+}
+
+pub(crate) fn detect() -> bool {
+    const M: u32 = 0x12345678;
+    unsafe {
+        pi_wait();
+        BUF.write_volatile(M);
+        pi_wait();
+        BUF.read_volatile() == M
+    }
+}
+
+pub(crate) fn text_out(s: &str) {
+    for chunk in s.as_bytes().chunks(CHUNK) {
+        pack(BUF, chunk);
+        pi_wait();
+        unsafe { WRITE_LEN.write_volatile(chunk.len() as u32) };
     }
 }

src/sc64.rs
@@ -1,0 +1,146 @@
+use crate::pi::Pi;
+use crate::cop0;
+use crate::memory_map::MemoryMap;
+
+const SC64_CART: u32 = 0x1000_0000;
+const DEBUG_TOP: u32 = 0x0400_0000;
+const USB_IO_RESERVE: u32 = 8 * 1024 * 1024;
+const DEBUG_OFF: u32 = DEBUG_TOP - USB_IO_RESERVE;
+const DBG_PHYS: u32 = SC64_CART + DEBUG_OFF;
+
+const REG_CMD: *mut u32 = 0xBFFF_0000 as *mut u32;
+const REG_D0: *mut u32 = 0xBFFF_0004 as *mut u32;
+const REG_D1: *mut u32 = 0xBFFF_0008 as *mut u32;
+const REG_ID: *mut u32 = 0xBFFF_000C as *mut u32;
+const REG_KEY: *mut u32 = 0xBFFF_0010 as *mut u32;
+
+const SR_CMD_BUSY: u32 = 1 << 31;
+const SR_CMD_ERR: u32 = 1 << 30;
+const USB_WRITE_STATUS_BUSY: u32 = 1 << 31;
+const CFG_ROM_WRITE_ENABLE: u32 = 1;
+const DATATYPE_TEXT: u32 = 1;
+const BLOCK: usize = 512;
+const POLL_MAX: u32 = 500_000;
+const DMA_ALIGN: usize = 2;
+
+#[repr(align(16))]
+struct Aligned {
+    bytes: [u8; BLOCK],
+}
+
+static mut DMA_BUF: Aligned = Aligned { bytes: [0; BLOCK] };
+
+#[inline(always)]
+fn pi_wait() {
+    while Pi::status().io_busy() {}
+}
+
+fn exec_cmd(cmd: u32, d0: Option<u32>, d1: Option<u32>) -> Result<[u32; 2], ()> {
+    unsafe {
+        match (d0, d1) {
+            (Some(a0), Some(a1)) => {
+                pi_wait();
+                REG_D0.write_volatile(a0);
+                pi_wait();
+                REG_D1.write_volatile(a1);
+            }
+            _ => {}
+        }
+        pi_wait();
+        REG_CMD.write_volatile(cmd);
+        let sr = loop {
+            pi_wait();
+            let s = REG_CMD.read_volatile();
+            if s & SR_CMD_BUSY == 0 {
+                break s;
+            }
+        };
+        if sr & SR_CMD_ERR != 0 {
+            return Err(());
+        }
+        pi_wait();
+        let r0 = REG_D0.read_volatile();
+        pi_wait();
+        let r1 = REG_D1.read_volatile();
+        Ok([r0, r1])
+    }
+}
+
+fn set_writable(enable: u32) -> Result<u32, ()> {
+    let r = exec_cmd(b'C' as u32, Some(CFG_ROM_WRITE_ENABLE), Some(enable))?;
+    Ok(r[1])
+}
+
+fn copy_to_dbg(off: usize, chunk: &[u8]) {
+    let n = chunk.len();
+    let d = (n + DMA_ALIGN - 1) & !(DMA_ALIGN - 1);
+    unsafe {
+        let dst = DMA_BUF.bytes.as_mut_ptr();
+        for i in 0..n {
+            dst.add(i).write_volatile(chunk[i]);
+        }
+        for i in n..d {
+            dst.add(i).write_volatile(0);
+        }
+        cop0::dcache_hit_writeback_invalidate_range(dst as usize, d);
+        let dram = MemoryMap::uncached_to_physical_mut(MemoryMap::uncached_mut(dst)) as u32;
+        Pi::set_dram_address(dram);
+        Pi::set_cart_address(DBG_PHYS + off as u32);
+        Pi::set_read_length(d as u32 - 1);
+        while Pi::status().dma_busy() {}
+    }
+}
+
+pub(crate) fn detect() -> bool {
+    unsafe {
+        pi_wait();
+        REG_KEY.write_volatile(0);
+        pi_wait();
+        REG_KEY.write_volatile(0x5F554E4C);
+        pi_wait();
+        REG_KEY.write_volatile(0x4F434B5F);
+        pi_wait();
+        REG_ID.read_volatile() == 0x53437632
+    }
+}
+
+pub(crate) fn text_out(s: &str) {
+    let b = s.as_bytes();
+    if b.is_empty() {
+        return;
+    }
+    let st = match exec_cmd(b'U' as u32, None, None) {
+        Ok(r) => r,
+        Err(_) => return,
+    };
+    if st[0] & USB_WRITE_STATUS_BUSY != 0 {
+        return;
+    }
+    let restore = match set_writable(1) {
+        Ok(v) => v,
+        Err(_) => return,
+    };
+    let mut off = 0usize;
+    while off < b.len() {
+        let n = core::cmp::min(BLOCK, b.len() - off);
+        copy_to_dbg(off, &b[off..off + n]);
+        off += n;
+    }
+    let _ = set_writable(restore);
+    let header = (DATATYPE_TEXT << 24) | (b.len() as u32 & 0xFFFFFF);
+    if exec_cmd(b'M' as u32, Some(DBG_PHYS), Some(header)).is_err() {
+        return;
+    }
+    let mut c = 0u32;
+    loop {
+        if c >= POLL_MAX {
+            break;
+        }
+        c += 1;
+        let r = exec_cmd(b'U' as u32, None, None);
+        match r {
+            Ok(v) if v[0] & USB_WRITE_STATUS_BUSY == 0 => break,
+            Ok(_) | Err(_) => {}
+        }
+    }
+}

thelemmy/nemu64-test

(cherry picked from commit 5023a9593ef9f400b8a3def647619913eba1a0d6)
Merge resolution: repointed debug output to the new central crate::text_out dispatcher
(emux/isviewer/sc64 backends) in tests/mod.rs and exception_handler.rs; took the refactored
isviewer.rs backend. CRITICAL FIX: the new isviewer.rs and sc64.rs declare their KSEG1 cart
registers as bare `0xB3FF0020 as *mut u32` etc., which under our 64-bit usize are not
sign-extended (0x0000_0000_B3FF_0020) -> isviewer::detect() faulted on the first print and the
screen went black on real hardware. Sign-extended all of them to 0xFFFF_FFFF_.... Validated on the
rig: on-screen output renders normally (no regression).

src/isviewer.rs
@@ -1,48 +1,50 @@
-use crate::memory_map::MemoryMap;
 use crate::pi::Pi;
 
-/// Write the length of the text here
-const ISVIEWER_WRITE_LEN: *mut u32 = MemoryMap::addr32_to_usize(0xB3FF0014) as *mut u32;
+const WRITE_LEN: *mut u32 = 0xFFFF_FFFF_B3FF_0014 as *mut u32;
+const BUF: *mut u32 = 0xFFFF_FFFF_B3FF_0020 as *mut u32;
+const CHUNK: usize = 0x200;
 
-// Write text data into this buffer
-const ISVIEWER_BUFFER_START: *mut u32 = MemoryMap::addr32_to_usize(0xB3FF0020) as *mut u32;
-const ISVIEWER_BUFFER_LENGTH: usize = 0x200;
-
+#[inline(always)]
 fn pi_wait() {
     while Pi::status().io_busy() {}
 }
 
-// This method simply prints text without synchronization. This should only be used from within
-// the exception handler which can't wait for a lock
-pub fn text_out(s: &str) {
-    if crate::emux::text_out(s) {
-        return;
-    }
-
-    for chunk in s.as_bytes().chunks(ISVIEWER_BUFFER_LENGTH) {
-        // Write
-        let mut value = 0u32;
-        let mut shift = 24u32;
-        let mut i = 0;
-        for byte in chunk {
-            value |= (*byte as u32) << shift;
-            if shift == 0 {
-                pi_wait();
-                unsafe { ISVIEWER_BUFFER_START.add(i).write_volatile(value) };
-                i += 1;
-                shift = 24;
-                value = 0;
-            } else {
-                shift -= 8;
-            }
-        }
-        if shift < 24 {
+fn pack(dst: *mut u32, chunk: &[u8]) {
+    let mut v = 0u32;
+    let mut sh = 24;
+    let mut i = 0usize;
+    for &b in chunk {
+        v |= (b as u32) << sh;
+        if sh == 0 {
             pi_wait();
-            unsafe { ISVIEWER_BUFFER_START.add(i).write_volatile(value) };
+            unsafe { dst.add(i).write_volatile(v) };
+            i += 1;
+            sh = 24;
+            v = 0;
+        } else {
+            sh -= 8;
         }
+    }
+    if sh < 24 {
         pi_wait();
-        unsafe {
-            ISVIEWER_WRITE_LEN.write_volatile(chunk.len() as u32);
-        }
+        unsafe { dst.add(i).write_volatile(v) };
+    }
+}
+
+pub(crate) fn detect() -> bool {
+    const M: u32 = 0x12345678;
+    unsafe {
+        pi_wait();
+        BUF.write_volatile(M);
+        pi_wait();
+        BUF.read_volatile() == M
+    }
+}
+
+pub(crate) fn text_out(s: &str) {
+    for chunk in s.as_bytes().chunks(CHUNK) {
+        pack(BUF, chunk);
+        pi_wait();
+        unsafe { WRITE_LEN.write_volatile(chunk.len() as u32) };
     }
 }

src/sc64.rs
@@ -1,0 +1,146 @@
+use crate::cop0;
+use crate::memory_map::MemoryMap;
+use crate::pi::Pi;
+
+const SC64_CART: u32 = 0x1000_0000;
+const DEBUG_TOP: u32 = 0x0400_0000;
+const USB_IO_RESERVE: u32 = 8 * 1024 * 1024;
+const DEBUG_OFF: u32 = DEBUG_TOP - USB_IO_RESERVE;
+const DBG_PHYS: u32 = SC64_CART + DEBUG_OFF;
+
+const REG_CMD: *mut u32 = 0xFFFF_FFFF_BFFF_0000 as *mut u32;
+const REG_D0: *mut u32 = 0xFFFF_FFFF_BFFF_0004 as *mut u32;
+const REG_D1: *mut u32 = 0xFFFF_FFFF_BFFF_0008 as *mut u32;
+const REG_ID: *mut u32 = 0xFFFF_FFFF_BFFF_000C as *mut u32;
+const REG_KEY: *mut u32 = 0xFFFF_FFFF_BFFF_0010 as *mut u32;
+
+const SR_CMD_BUSY: u32 = 1 << 31;
+const SR_CMD_ERR: u32 = 1 << 30;
+const USB_WRITE_STATUS_BUSY: u32 = 1 << 31;
+const CFG_ROM_WRITE_ENABLE: u32 = 1;
+const DATATYPE_TEXT: u32 = 1;
+const BLOCK: usize = 512;
+const POLL_MAX: u32 = 500_000;
+const DMA_ALIGN: usize = 2;
+
+#[repr(align(16))]
+struct Aligned {
+    bytes: [u8; BLOCK],
+}
+
+static mut DMA_BUF: Aligned = Aligned { bytes: [0; BLOCK] };
+
+#[inline(always)]
+fn pi_wait() {
+    while Pi::status().io_busy() {}
+}
+
+fn exec_cmd(cmd: u32, d0: Option<u32>, d1: Option<u32>) -> Result<[u32; 2], ()> {
+    unsafe {
+        match (d0, d1) {
+            (Some(a0), Some(a1)) => {
+                pi_wait();
+                REG_D0.write_volatile(a0);
+                pi_wait();
+                REG_D1.write_volatile(a1);
+            }
+            _ => {}
+        }
+        pi_wait();
+        REG_CMD.write_volatile(cmd);
+        let sr = loop {
+            pi_wait();
+            let s = REG_CMD.read_volatile();
+            if s & SR_CMD_BUSY == 0 {
+                break s;
+            }
+        };
+        if sr & SR_CMD_ERR != 0 {
+            return Err(());
+        }
+        pi_wait();
+        let r0 = REG_D0.read_volatile();
+        pi_wait();
+        let r1 = REG_D1.read_volatile();
+        Ok([r0, r1])
+    }
+}
+
+fn set_writable(enable: u32) -> Result<u32, ()> {
+    let r = exec_cmd(b'C' as u32, Some(CFG_ROM_WRITE_ENABLE), Some(enable))?;
+    Ok(r[1])
+}
+
+fn copy_to_dbg(off: usize, chunk: &[u8]) {
+    let n = chunk.len();
+    let d = (n + DMA_ALIGN - 1) & !(DMA_ALIGN - 1);
+    unsafe {
+        let dst = DMA_BUF.bytes.as_mut_ptr();
+        for i in 0..n {
+            dst.add(i).write_volatile(chunk[i]);
+        }
+        for i in n..d {
+            dst.add(i).write_volatile(0);
+        }
+        cop0::dcache_hit_writeback_invalidate_range(dst as usize, d);
+        let dram = MemoryMap::uncached_to_physical_mut(MemoryMap::uncached_mut(dst)) as u32;
+        Pi::set_dram_address(dram);
+        Pi::set_cart_address(DBG_PHYS + off as u32);
+        Pi::set_read_length(d as u32 - 1);
+        while Pi::status().dma_busy() {}
+    }
+}
+
+pub(crate) fn detect() -> bool {
+    unsafe {
+        pi_wait();
+        REG_KEY.write_volatile(0);
+        pi_wait();
+        REG_KEY.write_volatile(0x5F554E4C);
+        pi_wait();
+        REG_KEY.write_volatile(0x4F434B5F);
+        pi_wait();
+        REG_ID.read_volatile() == 0x53437632
+    }
+}
+
+pub(crate) fn text_out(s: &str) {
+    let b = s.as_bytes();
+    if b.is_empty() {
+        return;
+    }
+    let st = match exec_cmd(b'U' as u32, None, None) {
+        Ok(r) => r,
+        Err(_) => return,
+    };
+    if st[0] & USB_WRITE_STATUS_BUSY != 0 {
+        return;
+    }
+    let restore = match set_writable(1) {
+        Ok(v) => v,
+        Err(_) => return,
+    };
+    let mut off = 0usize;
+    while off < b.len() {
+        let n = core::cmp::min(BLOCK, b.len() - off);
+        copy_to_dbg(off, &b[off..off + n]);
+        off += n;
+    }
+    let _ = set_writable(restore);
+    let header = (DATATYPE_TEXT << 24) | (b.len() as u32 & 0xFFFFFF);
+    if exec_cmd(b'M' as u32, Some(DBG_PHYS), Some(header)).is_err() {
+        return;
+    }
+    let mut c = 0u32;
+    loop {
+        if c >= POLL_MAX {
+            break;
+        }
+        c += 1;
+        let r = exec_cmd(b'U' as u32, None, None);
+        match r {
+            Ok(v) if v[0] & USB_WRITE_STATUS_BUSY == 0 => break,
+            Ok(_) | Err(_) => {}
+        }
+    }
+}